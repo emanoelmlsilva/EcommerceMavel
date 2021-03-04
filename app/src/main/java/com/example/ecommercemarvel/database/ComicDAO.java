a73d1c8216734978e5cbbeb05fa9ef43aa6d6011
emanoel.silva <emanoel.silva@dcx.ufpb.br>
2021-03-04 11:52:10 -0300

    criar classe de entidade e de implementação do banco

---
+++ app/src/main/java/com/example/ecommercemarvel/database/ComicDAO.java
@@ -0,0 +1,18 @@
+package com.example.ecommercemarvel.database;
+
+import io.requery.Entity;
+import io.requery.Key;
+import io.requery.Persistable;
+
+@Entity
+public interface ComicDAO extends Persistable {
+
+    @Key
+    int getId();
+
+    String getTitle();
+    String getDescription();
+    String getImageUrl();
+    Float getPrice();
+    int getAmount();
+}
