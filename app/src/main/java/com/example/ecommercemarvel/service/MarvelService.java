93dd03e890d4f82691ee4e0a9d585e10a2283cab
emanoel.silva <emanoel.silva@dcx.ufpb.br>
2021-03-05 17:22:36 -0300

    adicionar parametro para limit de quadrinhos

--- app/src/main/java/com/example/ecommercemarvel/service/MarvelService.java
+++ app/src/main/java/com/example/ecommercemarvel/service/MarvelService.java
@@ -3,8 +3,9 @@ import com.example.ecommercemarvel.model.ResponseComic;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface MarvelService {
-    @GET("comics")
-    Call<ResponseComic> getComics();
+    @GET("comics?limit={limit}")
+    Call<ResponseComic> getComics(@Path("limit") int limit);
 }
