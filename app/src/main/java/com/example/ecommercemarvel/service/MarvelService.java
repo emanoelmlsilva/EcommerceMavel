974c4aa65e84add863be58f56cb74d309b2acfa1
emanoel.silva <emanoel.silva@dcx.ufpb.br>
2021-03-08 15:53:59 -0300

    alterar propriedader path para query

--- app/src/main/java/com/example/ecommercemarvel/service/MarvelService.java
+++ app/src/main/java/com/example/ecommercemarvel/service/MarvelService.java
@@ -3,9 +3,9 @@ import com.example.ecommercemarvel.model.ResponseComic;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface MarvelService {
-    @GET("comics?limit={limit}")
-    Call<ResponseComic> getComics(@Path("limit") int limit);
+    @GET("comics")
+    Call<ResponseComic> getComics(@Query("limit") int limit);
 }
